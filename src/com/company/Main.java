be915a6ea1270b85f389f7eb42129f75bb396d55
AlexF2134 <alexeyf1@yandex.ru>
2022-02-13 13:05:49 +0500

    " Initial "

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int bankCount = 2_000_000_000;
+        int bankTransfer = 500_000_000;
+        int totalSum = bankCount + bankTransfer;
+        System.out.println(totalSum);
+    }
+}
